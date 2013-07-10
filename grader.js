71669213c494e56f1de76913676fc17945573ecf
Ed DeMatta <edematta@hotmail.com>
2013-07-10 01:54:58 +0000

    revised grader

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "urlfile.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,27 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url) {
+       rest.get(program.url).on('complete', function(result) {
+         if (result instanceof Error) {
+           console.log("Invalid URL. Exiting");
+           process.exit(1);
+         } else {
+	   fs.writeFileSync(URLFILE_DEFAULT, result);  
+           var checkJsonU = checkHtmlFile(URLFILE_DEFAULT, program.checks);
+           var outJsonU = JSON.stringify(checkJsonU, null, 4);
+           console.log(outJsonU); 
+         }
+       }); 
+    } else {         
+       var checkJson = checkHtmlFile(program.file, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+    }
+          
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
